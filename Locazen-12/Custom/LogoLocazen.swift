3e6792e0ef64e973a2ea6c4215f75d45aa771b16
SofyanZa <soza@live.fr>
2023-02-12 13:01:33 +0100

    add LogoLocazen

---
+++ Locazen-12/Custom/LogoLocazen.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImagr.swift
+//  TestApp
+//
+//  Created by SofyanZ on 07/02/2023.
+//
+
+import SwiftUI
+
+struct LogoLocazen: View {
+    var body: some View {
+        Image("recad")
+            .offset(y: -60)
+            .padding(.bottom, -75)
+            .padding()
+    }
+}
+
+struct LogoLocazen_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoLocazen()
+    }
+}
