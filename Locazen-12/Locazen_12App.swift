8e8f298505dcb0e6bad978a3fabf6d98df671a79
SofyanZa <soza@live.fr>
2023-02-12 12:50:55 +0100

    Initial Commit

---
+++ Locazen-12/Locazen_12App.swift
@@ -0,0 +1,17 @@
+//
+//  Locazen_12App.swift
+//  Locazen-12
+//
+//  Created by SofyanZ on 12/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct Locazen_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
