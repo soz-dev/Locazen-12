69a21fa0b45b5055d473b6eaf090b6eb2aa3bc40
SofyanZa <soza@live.fr>
2023-02-12 12:57:14 +0100

    add custom color

---
+++ Locazen-12/Custom/CustomColor.swift
@@ -0,0 +1,14 @@
+//
+//  CustomColor.swift
+//  TestApp
+//
+//  Created by SofyanZ on 06/02/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct CustomColor {
+    static let myColorBlue = Color("colorBlueLocaz")
+    static let myColorPink = Color("colorPinkLocaz")
+}
