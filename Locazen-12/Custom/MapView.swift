a7945e336d94d08a6aa7d0200a72b40a636fb1be
SofyanZa <soza@live.fr>
2023-02-12 12:55:35 +0100

    add mapview

---
+++ Locazen-12/Custom/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  TestApp
+//
+//  Created by SofyanZ on 07/02/2023.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 43.400000, longitude: 3.683333),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+
+    var body: some View {
+        Map(coordinateRegion: $region)
+            .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
