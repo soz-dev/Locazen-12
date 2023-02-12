528b303fd8d8a8379194b5e6ce40ceb9f080baa5
SofyanZa <soza@live.fr>
2023-02-12 12:59:17 +0100

    add partners page

---
+++ Locazen-12/Pages/Partners.swift
@@ -0,0 +1,117 @@
+//
+//  Navigation.swift
+//  TestApp
+//
+//  Created by SofyanZ on 30/01/2023.
+//
+
+import SwiftUI
+
+struct Partners: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image("partners")
+                    .resizable()
+                    .scaledToFill()
+                    .border(Color(UIColor.darkGray))
+                    .aspectRatio(contentMode: .fit)
+                
+                LogoLocazen()
+                
+                Divider()
+                    .padding()
+                
+                VStack {
+                    Text("Nos Partenaires")
+                        .padding()
+                        .background(CustomColor.myColorBlue)
+                        .border(.black)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+                
+                HStack() {
+                    Text("Nos partenaires nous accompagnent dans nos oeuvres en nous soutenant. \n Vous pouvez visiter leur site web en cliquant sur les images ci dessous.")
+                        .lineLimit(6)
+                        .multilineTextAlignment(.center)
+                }
+                .padding()
+                
+                VStack {
+                    Link(destination: URL(string: "https://www.sete.fr/")!) {
+                        Image("partner1")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 250, height: 220)
+                            .clipped()
+                            .overlay(alignment: .bottomLeading) {
+                                HStack {
+                                    Spacer()
+                                    Image(systemName: "arrow.up.forward.app.fill")
+                                }
+                                .padding()
+                                .foregroundColor(.black)
+                                .font(.title2.weight(.semibold))
+                            }
+                            .mask {
+                                RoundedRectangle(cornerRadius: 14, style: .continuous)
+                            }
+                            .padding(3)
+                            .background(alignment: .bottom) {
+                                LinearGradient(gradient: Gradient(colors: [Color(.black), Color(.displayP3, red: 183/255, green: 167/255, blue: 149/255)]), startPoint: .leading, endPoint: .trailing)
+                                    .mask {
+                                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                                    }
+                            }
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .clipped()
+                        
+                    }
+                    Link(destination: URL(string: "https://www.agglopole.fr/")!) {
+                        Image("partner2")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 250, height: 220)
+                            .clipped()
+                            .overlay(alignment: .bottomLeading) {
+                                HStack {
+                                    Spacer()
+                                    Image(systemName: "arrow.up.forward.app.fill")
+                                }
+                                .padding()
+                                .foregroundColor(.black)
+                                .font(.title2.weight(.semibold))
+                            }
+                            .mask {
+                                RoundedRectangle(cornerRadius: 14, style: .continuous)
+                            }
+                            .padding(3)
+                            .background(alignment: .bottom) {
+                                LinearGradient(gradient: Gradient(colors: [Color(.black), Color(.displayP3, red: 183/255, green: 167/255, blue: 149/255)]), startPoint: .leading, endPoint: .trailing)
+                                    .mask {
+                                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                                    }
+                            }
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .clipped()
+                    }
+                    
+                }
+                .padding()
+            }
+            .background(.white)
+        }
+        .foregroundColor(.black)
+    }
+}
+
+struct Partners_Previews: PreviewProvider {
+    static var previews: some View {
+        Partners()
+    }
+}
