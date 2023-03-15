e75d44d9462556738d3ed7bd9e002eff561a8252
SofyanZa <soza@live.fr>
2023-03-15 01:17:01 +0100

    add LocationDetailView

---
+++ Locazen-12/View/LocationDetailView.swift
@@ -0,0 +1,121 @@
+//
+//  LocationDetailView.swift
+//
+//  Created by Sofyan Zarouri on 14/03/2023.
+//  Copyryght © 2023 Sofyan Zarouri. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationDetailView: View {
+    
+    @EnvironmentObject private var vm: LocationsViewModel
+    
+    let location: Location
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                imageSection
+                    .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y:10)
+                
+                VStack(alignment: .leading, spacing: 16.0) {
+                    titleSection
+                    Divider()
+                    descriptionSection
+                    Divider()
+                    mapLayer
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+        }
+        .ignoresSafeArea()
+        .background(.ultraThinMaterial)
+        .overlay(backButton, alignment: .topLeading)
+    }
+}
+
+struct LocationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationDetailView(location: LocationsDataService.locations.first!)
+            .environmentObject(LocationsViewModel())
+    }
+}
+
+
+extension LocationDetailView {
+    
+    private var imageSection: some View {
+        TabView {
+            ForEach(location.imageNames, id: \.self) {
+                Image($0)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width)
+                    .clipped()
+            }
+        }
+        .frame(height: 500)
+        .tabViewStyle(PageTabViewStyle())
+    }
+    
+    private var titleSection: some View {
+        
+        VStack(alignment: .leading, spacing: 8) {
+            Text(location.name)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            Text(location.cityName)
+                .font(.title3)
+                .foregroundColor(.secondary)
+        }
+    }
+    
+    private var descriptionSection: some View {
+        
+        VStack(alignment: .leading, spacing: 8) {
+            Text(location.description)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
+            if let url = URL(string: location.link) {
+                Link("Lire plus de détails ...", destination: url)
+                    .font(.headline)
+                    .tint(.blue)
+                
+            }
+        }
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: .constant(MKCoordinateRegion(
+            center: location.coordinates, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        )), annotationItems: [location]) { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .shadow(radius: 10)
+            }
+        }
+        .allowsHitTesting(false)
+        .aspectRatio(1, contentMode: .fit)
+        .cornerRadius(30)
+    }
+    
+    private var backButton: some View {
+        Button {
+            vm.sheetLocation = nil
+        } label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+                .padding(16)
+                .foregroundColor(.primary)
+                .background(.thickMaterial)
+                .cornerRadius(10)
+                .shadow(radius: 4)
+                .padding()
+        }
+        
+    }
+}
