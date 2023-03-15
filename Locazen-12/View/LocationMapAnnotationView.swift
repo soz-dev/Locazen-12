51fc92017a439706eb52d14228a565e278b997bb
SofyanZa <soza@live.fr>
2023-03-15 01:12:40 +0100

    add LocationMapAnnotationView

---
+++ Locazen-12/View/LocationMapAnnotationView.swift
@@ -0,0 +1,47 @@
+//
+//  LocationMapAnnotationView.swift
+//
+//  Created by Sofyan Zarouri on 14/03/2023.
+//  Copyryght © 2023 Sofyan Zarouri. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct LocationMapAnnotationView: View {
+    
+    let accentColor = Color("AccentColor")
+    
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "map.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(6)
+                .background(accentColor)
+                .cornerRadius(30)
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(accentColor)
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+                .padding(.bottom, 35)
+        }
+    }
+}
+
+struct LocationMapAnnotationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            LocationMapAnnotationView()
+        }
+    }
+}
